ffa4878d5954bb432717199b1254575934f04708
Nick Franciosi <nickfranciosi@gmail.com>
2016-03-12 23:11:48 -0600

    added boilerplate for new routes

---
+++ app/pages/learnAdd.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LearnAdd = () => {
+  return(
+    <h2>Display forms to add items</h2>
+  );
+};
+
+export default LearnAdd;
---
+++ app/pages/learnList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LearnList = () => {
+  return(
+    <h2>Display all items</h2>
+  );
+};
+
+export default LearnList;
